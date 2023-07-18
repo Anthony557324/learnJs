18a37a220eaa7f3ccaa3c6e10d1c9759cd5d4a18
Anthony557324 <1621860255@qq.com>
2023-07-18 14:43:21 +0800

    feat:JavaScript练习

---
+++ compare.js
@@ -0,0 +1,5 @@
+console.log(undefined === null)
+console.log(undefined == null)
+
+console.log('1' + 0)    //结果为10(string类型)
+console.log('1' - 1)    //结果为0(number类型)
---
+++ strict.js
@@ -0,0 +1,4 @@
+'use strict';
+//严格模式 会限制作用域
+
+console.log(this);
