22539e3a2b3b2f5d00842d24dbd6a5e9f407c75c
Anthony557324 <1621860255@qq.com>
2023-07-22 20:44:29 +0800

    feat:二叉树

---
+++ data-structure/tree.js
@@ -0,0 +1,23 @@
+const treeNode = {
+  val: 1,
+  left: null,
+  right: null,
+};
+
+const treeNode2 = {
+  val: 1,
+  left: null,
+  right: null,
+};
+
+const treeRoot = {
+    val: 1,
+    left: treeNode,
+    right:treeNode2
+}
+
+// 树的遍历
+// 前序遍历
+// 中序遍历 - 二叉搜索树中序遍历结果是递增序列
+// 后序遍历
+// 层序遍历
