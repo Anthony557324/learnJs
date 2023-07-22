a07e3c214f18d28a38113fe3f421e4f77a8b6032
Anthony557324 <1621860255@qq.com>
2023-07-22 21:30:34 +0800

    feat:栈和队列

---
+++ data-structure/queue.js
@@ -0,0 +1,8 @@
+// 队列 push shift
+const queue = []
+
+queue.push(1)
+queue.push(2)
+const el = queue.shift()
+console.log(el);
+console.log(queue);
---
+++ data-structure/stack.js
@@ -0,0 +1,9 @@
+// 栈   push pop
+const stack = []
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+const el = stack.pop()
+console.log(el);
+console.log(stack);
