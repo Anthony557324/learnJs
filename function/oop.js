e9cd7e0090588479afc4f8a4c4d1a462865f2c31
Anthony557324 <1621860255@qq.com>
2023-07-25 16:12:29 +0800

    feat:this练习

---
+++ function/oop.js
@@ -0,0 +1,57 @@
+// JS中的面向对象编程
+// 1.封装（隐藏数据，暴露方法）
+// 2.继承
+// 函数作为构造函数，函数中包括属性和方法
+function Person(name, age) {
+    // 构造函数中的this指向实例对象
+    this.name = name;
+    this.age = age;
+    this.getName = function () {
+        console.log(this.name);
+        return this.name;
+    }
+    this.setName = function (name) {
+        this.name = name;
+    }
+}
+
+Person.prototype.who = function () {
+    console.log(this.name);
+}
+
+const obj1 = new Person('ljq', 22)
+obj1.setName('ceshi')
+
+// 写函数的时候尽量语义化你想要做的操作
+obj1.name = 'ceshi2'
+console.log(obj1.getName());
+obj1.who()
+
+// 请仿写数组的yourselfForeach方法
+const arr = [1, 2, 3]
+// TODO: arr.yourselfForeach((item) => {console.log(item)})
+// arr.forEach((item) => {console.log(item)})
+
+Array.prototype.yourselfForeach = function (callback) {
+    console.log(this)
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+
+
+// 仿写数组的yourself Map方法
+
+Array.prototype.yourselfMap = function (callback) {
+    console.log(this)
+    const result = []
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i])
+        result.push(callback(this[i], i, this))
+    }
+    return result;
+}
+
+arr.yourselfForeach((item) => { console.log(item) })
+const result = arr.yourselfMap(item => item + 1)
+console.log(result);
