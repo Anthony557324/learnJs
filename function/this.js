688bb231985009f3491d0eedbecb2b8580bb657f
Anthony557324 <1621860255@qq.com>
2023-07-27 16:17:12 +0800

    feat:this方法

--- function/this.js
+++ function/this.js
@@ -13,11 +13,20 @@ const obj = {
 // 非严格模式下
 // 1.就近原则，谁调用的这个函数，this就指向谁
 // 把this理解为函数的隐藏参数，将函数的调用者作为this的值传入
+// 2.构造函数中的this指向实例对象
 // 严格模式下
 // 函数的直接调用，this指向undefined
 obj.sayHi()
-// sayHi([this = obj])
+// sayHi({thisInner=obj})
+
 const obj2 = {
     name: 'obj2',
     sayHi: obj.sayHi
 }
+
+obj2.sayHi()
+// syaHi([thisInner=obj2])
+
+const sayHi = obj.sayHi
+sayHi()
+// sayHi([thisInner=window])
